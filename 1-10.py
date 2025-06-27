403dcb0435e07550a783775a1212dcd457ef8dc2
Xuswnss <cooconut0226@gmail.com>
2025-06-27 17:58:48 +0900

    feat(6/27) #1~ #10

---
+++ 1-10.py
@@ -0,0 +1,44 @@
+#1
+print('#1 hello World 문자 출력')
+print('hello world')
+print('- '* 10)
+#2
+print("#2 화면에 Mary's cosmetics을 출력하세요. (중간에 '가 있음에 주의하세요)")
+print("Mary's cosmetics")
+print('- '* 10)
+#3
+print('#3 화면에 아래 문장을 출력하세요. (중간에 "가 있음에 주의하세요.) -> 신씨가 소리질렀다. "도둑이야".')
+print('신씨가 소리질렀다. "도둑이야')
+print('- '* 10)
+#4
+print('#4 화면에 C:\Windows를 출력하세요.')
+print('C:\windows')
+print('- '* 10)
+#5
+print('#5 print("안녕하세요.\n만나서\t\t반갑습니다."). ')
+# \n -> 줄바꿈 \t -> 탭
+print('- '* 10)
+#6
+print("#6 함수에 두 개의 단어를 입력한 예제입니다. 아래 코드의 출력 결과를 예상해봅시다.")
+print("오늘은", "일요일")
+print('- '* 10)
+#7
+print('#7 naver;kakao;sk;samsung -> 다음과 같이 출력하세요')
+print('naver;kakao;sk;samsung')
+print('- '* 10)
+
+#8
+print('#8. 다음과 함께 출력하세요')
+print('naver/kakao/sk/samsung')
+print('- '* 10)
+
+#9
+## print 함수는 기본적으로 출력 후 자동으로 줄바꿈(개행)을 추가한다. 그러나 end 매개변수를 
+# 사용하여 다른 문자열을 지정할 수 있다. end ="" 은 출력 후 아무것도 추가하지 않도록 설정한다.
+print("#9 줄바꿈")
+print("first", end="");print("second")
+print('- '* 10)
+#10
+print('#10 5/3의 결과를 화면에 출력하세요.')
+print(5/3)
+print('- '* 10)
