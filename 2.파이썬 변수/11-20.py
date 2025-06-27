7414b483cec204550f8d1bccc10dc45e9b6a1498
Xuswnss <cooconut0226@gmail.com>
2025-06-28 00:45:11 +0900

    feat(6/28) # 11 -#14

---
+++ 2.파이썬 변수/11-20.py
@@ -0,0 +1,25 @@
+#11
+print('#11 삼성전자라는 변수로 50,000원을 바인딩 해보세요. 삼성전자 주식 10주를 보유하고 있을 때 총 평가금액을 출력하세요')
+samsung = 50000
+price = samsung * 10
+print('총 평가금액',price)
+print('- '* 10)
+#12
+print('#12 삼성전자의 일부 투자 정보입니다. 변수를 사용해서 시가총액, 현재가, per등을 바인딩 해보세요')
+marketCapitalization = 298000000000
+currentPrice = 50000
+per = 15.79
+print('시가총액', marketCapitalization)
+print('현재가', currentPrice)
+print('PER', per)
+print('- '* 10)
+
+#13
+print('#13 문자열 출력')
+s = 'hello'
+t = 'python'
+print(s+ "!" ,t)
+print('- '* 10)
+#14
+print('#14 파이썬을 이용한 값 계산')
+print(2+2*3)
