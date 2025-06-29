d479ff0ce523210a12fd152e693697be770c233e
Xuswnss <cooconut0226@gmail.com>
2025-06-30 00:03:12 +0900

    feat(6/30) #15-#20

--- 2.파이썬 변수/11-20.py
+++ 2.파이썬 변수/11-20.py
@@ -23,3 +23,49 @@ print('- '* 10)
 #14
 print('#14 파이썬을 이용한 값 계산')
 print(2+2*3)
+print('- '* 10)
+
+#15
+print('#15 type 함수')
+a= 2
+print(type(a))
+print('- '* 10)
+
+#16
+print('#16 string to Int')
+num_str = "720"
+num_int = int(num_str) # string to integer
+print(num_int+1, type(num_int))
+
+print('- '* 10)
+
+#17
+print('#17 integer to string')
+num = 100
+num_str = str(num)
+print(num_str, type(num_str))
+print('- '* 10)
+
+#18
+print('#18 String to float')
+data = "15.79"
+data = float(data)
+print(data, type(data))
+print('- '* 10)
+
+#19
+print('#19 문자열 정수 변환')
+year = "2024"
+year_int = int(2024)
+print(year_int -1)
+print(year_int -2)
+print(year_int -3)
+
+print('- '* 10)
+
+#20
+print('#20 무이자 36개월치 계산')
+month = 48584 
+result = month * 36
+print(result)
+print('- '* 10)
