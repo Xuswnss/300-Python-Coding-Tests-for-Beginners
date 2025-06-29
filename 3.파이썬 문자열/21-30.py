02590fe2999629473e51542a1b3a4800440fcbdc
Xuswnss <cooconut0226@gmail.com>
2025-06-30 08:23:59 +0900

    feat(6/30) #21-#30

--- 3.파이썬 문자열/21-30.py
+++ 3.파이썬 문자열/21-30.py
@@ -26,3 +26,52 @@ print('#24 문자열 역순 출력 ')
 string = "PYTHON"
 print(string[::-1])
 print('- '*10)
+
+#25
+print('#25. 문자열 치환')
+num = '010-1111-2222'
+new_num = num.replace('-',' ')
+print(new_num)
+print('- '*10)
+
+#26
+print('#26 문자열 치환 ; replace a string')
+num = '010-1111-2222'
+new_num = num.replace('-','')
+print(new_num)
+print('- '*10)
+
+#27
+print('#27 도메인 출력')
+url = "http://sharebook.kr"
+new_url = url.split('.') # -> 문자열 배열 출력
+print(new_url[-1])
+print('- '*10)
+
+#28
+print('#28 문자열은 immutable')
+# lang = 'python'
+# lang[0] = 'P'
+# print(lang)
+print('문자열은 assignment(할당)메서드를 지원하지 않기 때문에 수정할 수 없다')
+print('- '*10)
+
+#29
+print('#29 replace method')
+string = 'abcdefagdafsal'
+new_str = string.replace('a',"A")
+print(new_str)
+print('- '*10)
+#30
+print('#30. replace method')
+string = 'abcd'
+string.replace('b',"b")
+#replace는 새로운 객체를 리턴하므로 원본은 변하지 않음
+new_str = string.replace('b', 'B')
+print(string)
+print(new_str)
+print('- '*10)
+
+
+
+
