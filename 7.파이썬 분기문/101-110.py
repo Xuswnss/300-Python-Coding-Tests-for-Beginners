5295b20a4a3038fe6bc1de904b8bb76aa44da8cf
Xuswnss <cooconut0226@gmail.com>
2025-06-30 19:05:41 +0900

    feat(6/30) : #101-#110

---
+++ 7.파이썬 분기문/101-110.py
@@ -0,0 +1,46 @@
+#101
+print('#101 파이썬에서 True or False를 가진 데이터 타입은?>')
+print('bool type')
+print('- '*10)
+
+#102
+print('#102 Predict the output of the code. ')
+print(3 == 5)
+print('- '*10)
+
+
+#103
+print('#103 Predict the output of the code. ')
+print(3 < 5)
+print('- '*10)
+
+#104
+print('#104 Predict the output of the code. ')
+x = 4
+print(1 < x < 5)
+
+print('- '*10)
+
+#104
+print('#104 Predict the output of the code. ')
+x = 4
+print(1 < x < 5)
+
+print('- '*10)
+
+#105
+print('#105 Predict the output of the code.')
+print ((3 == 3) and (4 != 3))
+print('- '*10)
+
+#106
+print('#106 Predict the output of the code.')
+# print(3 => 4)
+# 지원하지 않은 연산자
+print('- '*10)
+
+#107
+print('#107 Predict the output of the code.')
+if 4 < 3: #false
+    print("Hello World")
+    
