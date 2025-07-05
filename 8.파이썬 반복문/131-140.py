322a03eec7767905ea3214c9a61f6032e5240891
Xuswnss <cooconut0226@gmail.com>
2025-07-05 22:23:03 +0900

    feat(7/5) #131 - #140

---
+++ 8.파이썬 반복문/131-140.py
@@ -0,0 +1,53 @@
+#131
+print('#131')
+fruits = [ 'apple', 'orange', 'watermelon']
+for i in fruits:
+    print(i)
+print('- '*10)
+
+#132
+print('#132')
+fruits = [ 'apple', 'orange', 'watermelon']
+for i in fruits:
+    print('###')
+print('- '*10)
+
+#133
+print('#133')
+for i in ["A", "B","C"]:
+    print(i)
+print('- '*10)
+
+#134
+print('#134')
+for 변수 in ["A", "B", "C"]:
+  print("출력:", 변수)
+print('- '*10)
+
+#135
+print('#135')
+for 변수 in ["A", "B", "C"]:
+  b = 변수.lower()
+  print("변환:", b)
+print('- '*10)
+
+#136 - 137
+print('#136-#137')
+array = [10, 20, 30]
+for i in array:
+    print(i)
+print('- '*10)
+
+
+#138
+print('#138')
+for i in array:
+    print(i)
+    print("------")
+print('- '*10)    
+    
+#138
+print('#139')
+print('-------')
+for i in array:
+    print(i)
