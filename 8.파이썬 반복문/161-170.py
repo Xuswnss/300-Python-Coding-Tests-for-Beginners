e3e3b42906c04cbf0e6a8fbf4e66a506f1b579a0
Xuswnss <cooconut0226@gmail.com>
2025-07-05 22:23:58 +0900

    feat(7/5): #161 - #170

---
+++ 8.파이썬 반복문/161-170.py
@@ -0,0 +1,66 @@
+#161
+for i in range(0,100):
+    print(i)
+print('- '*10)
+
+#162
+for x in range(2002,2051,4):
+    print(x)
+print('- '*10)
+
+#163
+print('#163')
+for i in range(3,31,3):
+    print(i)
+print('- '*10)
+
+#164
+print('#164')
+for i in range(100):
+    print(99 - i)
+print('- '*10)
+
+#165
+print('#165')    
+for i in range(10):
+    print(i / 10)
+print('- '*10)
+
+#166
+print('#166')
+for i in range(1, 10):
+    print( ' 3 x', i , '=', 3*i)
+print('- '*10)   
+
+#167 
+print('#167') 
+for i in range(1, 10, 2):
+     print( ' 3 x', i , '=', 3*i)
+print('- '*10)  
+
+#168
+print('#168') 
+sum = 0  
+for i in range(1,11):
+    sum += i 
+
+print('합 :', sum)
+print('- '*10)  
+
+#169
+print('#169')
+sum = 0
+for i in range(1,10):
+    sum += i
+print('합 :', sum) 
+print('- '*10) 
+
+#170
+result = 1
+print('#170')  
+for i in range(1,11):
+    result = result * i
+    
+print(result)
+
+    
