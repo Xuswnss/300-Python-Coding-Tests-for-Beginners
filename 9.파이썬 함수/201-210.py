62d8c44cbc48b3a8de9d3f04a45eb20d71e12fc8
Xuswnss <cooconut0226@gmail.com>
2025-07-06 23:43:38 +0900

    feat(7/6) #201-#210

---
+++ 9.파이썬 함수/201-210.py
@@ -0,0 +1,23 @@
+#201
+print('#201')
+def print_coin():
+    print('비트코인')
+print('- '*10)
+
+#202
+print('#202')
+print_coin()
+print('- '*10)
+
+#203
+print('#203')
+for _ in range(100):
+    print_coin()
+print('- '*10)
+
+#204
+print('#204')
+def print_coins():
+    for _ in range(100):
+        print_coin()
+print('- '*10)
